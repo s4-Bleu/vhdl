be1d9257419836d7717a880e24c9c63f57ae0de8
arnaudlaf28 <arnaudlaf@hotmail.com>
2025-07-29 17:35:54 -0400

    update de actor

--- new/actor.vhd
+++ new/actor.vhd
@@ -26,17 +26,19 @@ use IEEE.NUMERIC_STD.ALL;
 entity actor is
     Port ( 
         i_clk             : in  STD_LOGIC;
---        i_reset           : in  STD_LOGIC;
+        i_reset           : in  STD_LOGIC;
         i_is_enable       : in STD_LOGIC;        
 
         -- Signaux de mise à jour
         i_pos_update_en   : in  STD_LOGIC;                          -- Active la mise à jour de la position
-        i_tile_update_en  : in  STD_LOGIC;                          -- Active la mise à jour de l'ID de tuile
-
-        -- Nouvelles valeurs à charger (position X/Y de l'acteur et nouvelle tuile) -- les positions des acteurs sont ancré en haut a gauche
         i_new_pos_x       : in  STD_LOGIC_VECTOR (9 downto 0);      -- Nouvelle position globale X de l'acteur (en pixels)
         i_new_pos_y       : in  STD_LOGIC_VECTOR (9 downto 0);      -- Nouvelle position globale Y de l'acteur (en pixels)
-        i_new_tile_id     : in  STD_LOGIC_VECTOR (3 downto 0);      -- Nouvelle tuile à afficher pour l'acteur
+        
+        
+        -- Nouvelles valeurs à charger (position X/Y de l'acteur et nouvelle tuile) -- les positions des acteurs sont ancré en haut a gauche
+       i_tile_update_en      : in  STD_LOGIC;                          -- Active la mise �  jour de l'ID de tuile
+       i_tile_pos_index      : in  STD_LOGIC_VECTOR(1 downto 0);     -- Indice (0 � 3)
+       i_new_tile_id         : in  STD_LOGIC_VECTOR (3 downto 0);      -- Nouvelle tuile à afficher pour l'acteur
 
         -- Pixel courant envoyé par le viewport (le pixel qui ce fait print)
         i_curr_px_x       : in  STD_LOGIC_VECTOR (9 downto 0);      -- Position X du pixel courant (à l'écran)
@@ -54,7 +56,11 @@ architecture Behavioral of actor is
     -- ELEMTENT DE MEMOIRE DE L'ACTEUR
     signal s_actor_pos_x: STD_LOGIC_VECTOR(9 downto 0) := (others => '0'); -- position de absolue de l'acteur X (coin en haut a gauche)
     signal s_actor_pos_y: STD_LOGIC_VECTOR(9 downto 0) := (others => '0'); -- position de absolue de l'acteur Y (coin en haut a gauche)
-    signal s_tile_id: STD_LOGIC_VECTOR(3 downto 0) := (others => '0'); -- la tuile de l'acteur
+    
+    type t_tile_id_array is array (0 to 3) of std_logic_vector(3 downto 0);
+    signal s_actor_tiles : t_tile_id_array := (others => (others => '0')); --Vecteur 2D de tuiles d'acteur. (bit0 correspond a (0,0), bit1 correspond a (0,1) etc,)
+    
+    --signal s_tile_id: STD_LOGIC_VECTOR(3 downto 0) := (others => '0'); -- la tuile de l'acteur
     
     -- FIN DES ELEMENTS DE MEMOIRE
 --    signal s_is_reset    : STD_LOGIC := '0';
@@ -64,53 +70,46 @@ architecture Behavioral of actor is
 
 begin
 
-check_actor_update_needed : process(i_clk)
-begin 
---    if i_reset = '1' then
---        s_actor_pos_x <= (others=>'0');
---        s_actor_pos_y <= (others=>'0');
---        s_tile_id     <= (others=>'0');
---        s_is_reset <= '1';
-        
+check_actor_update_needed : -- Processus de mise � jour
+process(i_clk)
+    variable index : integer;
+begin
     if rising_edge(i_clk) then
-        if i_pos_update_en = '1' and i_is_enable = '1' then
-            s_actor_pos_x <= i_new_pos_x;
-            s_actor_pos_y <= i_new_pos_y;
-        end if; 
-        
-        if i_tile_update_en = '1' and i_is_enable = '1' then
-            s_tile_id <= i_new_tile_id;
+        if i_is_enable = '1' then
+            if i_pos_update_en = '1' then
+                s_actor_pos_x <= i_new_pos_x;
+                s_actor_pos_y <= i_new_pos_y;
+            end if;
+
+            if i_tile_update_en = '1' then
+                index := to_integer(unsigned(i_tile_pos_index));
+                s_actor_tiles(index) <= i_new_tile_id;
+            end if;
         end if;
-        
---        s_is_reset <= '0';
     end if;
 end process;
 
-update_actor_output: process(i_curr_px_x, i_curr_px_y, s_actor_pos_x, s_actor_pos_y, s_tile_id) --s_is_reset
+-- Calcul de la sortie visible
+update_actor_output: process(i_curr_px_x, i_curr_px_y, s_actor_pos_x, s_actor_pos_y, s_actor_tiles)
     variable diff_x : unsigned(9 downto 0);
     variable diff_y : unsigned(9 downto 0);
+    variable tile_idx : integer;
 begin
-
---    if s_is_reset = '1' then
---        o_tile_px    <= (others => '0');
---        o_tile_py    <= (others => '0');
---        o_tile_id    <= (others => '0');
---        o_is_visible <= '0';
-    
-    -- on vérifier si le pixel courant est belle et bien dans la zone de l'acteur
     if (unsigned(i_curr_px_x) >= unsigned(s_actor_pos_x)) and
        (unsigned(i_curr_px_x) < unsigned(s_actor_pos_x) + largeur_acteur) and
        (unsigned(i_curr_px_y) >= unsigned(s_actor_pos_y)) and
        (unsigned(i_curr_px_y) < unsigned(s_actor_pos_y) + hauteur_acteur) then
-       
-       diff_x := unsigned(i_curr_px_x) - unsigned(s_actor_pos_x);
-       diff_y := unsigned(i_curr_px_y) - unsigned(s_actor_pos_y);
 
-        o_tile_px <= STD_LOGIC_VECTOR(diff_x(3 downto 0)); -- on peux ce le permettre car on c'est qu'on est dans la zone de l'acteur
+        diff_x := unsigned(i_curr_px_x) - unsigned(s_actor_pos_x);
+        diff_y := unsigned(i_curr_px_y) - unsigned(s_actor_pos_y);
+
+        o_tile_px <= STD_LOGIC_VECTOR(diff_x(3 downto 0));
         o_tile_py <= STD_LOGIC_VECTOR(diff_y(3 downto 0));
-        o_tile_id <= s_tile_id;
-        o_is_visible <= '1';
 
+        tile_idx := 2 * to_integer(unsigned(diff_y(4 downto 4))) + to_integer(unsigned(diff_x(4 downto 4)));
+
+        o_tile_id <= s_actor_tiles(tile_idx);
+        o_is_visible <= '1';
     else
         o_tile_px <= (others => '0');
         o_tile_py <= (others => '0');
--- new/actor_tile_buffer.vhd
+++ new/actor_tile_buffer.vhd
@@ -21,71 +21,95 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
+--use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity actor_tile_buffer is
-    Port ( i_clk : in STD_LOGIC;
---           i_reset : in STD_LOGIC;
-           i_tile_id : in STD_LOGIC_VECTOR (3 downto 0);
-           i_tile_read_px_x : in STD_LOGIC_VECTOR (3 downto 0);
-           i_tile_read_px_y : in STD_LOGIC_VECTOR (3 downto 0);
-           i_write_tile_id : in STD_LOGIC_VECTOR (3 downto 0);
-           i_write_tile_px_x : in STD_LOGIC_VECTOR (3 downto 0);
-           i_write_tile_px_y : in STD_LOGIC_VECTOR (3 downto 0);
-           i_write_buffer_px : in STD_LOGIC;
-           i_write_tile_pixel_color : in STD_LOGIC_VECTOR (3 downto 0);
-           o_color_code : out STD_LOGIC_VECTOR (3 downto 0));
-end actor_tile_buffer;
+entity Tile_buffer_actor is
+ Port (
+        i_clk        : in  std_logic;
+        i_reset      : in  std_logic;
 
-architecture Behavioral of actor_tile_buffer is
+        i_tile_id    : in  std_logic_vector(5 downto 0); --64 tuiles
+        i_x          : in  std_logic_vector(2 downto 0); 
+        i_y          : in  std_logic_vector(2 downto 0); 
+        
+        i_we         : in std_logic;
+        i_wr_x       : in  std_logic_vector(2 downto 0); 
+        i_wr_y       : in  std_logic_vector(2 downto 0); 
+        i_pixel_data : in std_logic_vector(3 downto 0); 
 
+        o_color_code : out std_logic_vector(3 downto 0)
+    );
+end Tile_buffer_actor;
 
-type tile_arr_actor_px is array(0 to 2047) of STD_LOGIC_VECTOR (3 downto 0);--8id * 16px_x * 16px_y (et le tableau stocke des couleurs sur 4 bits)
-signal s_tile_arr_actor_px : tile_arr_actor_px := ( others => (others => '0'));
+architecture Behavioral of Tile_buffer_actor is
 
-signal s_tiles_reading_index : std_logic_vector(10 downto 0);   -- 10 downto 8 => id (3bits), 7 downto 4 => px_y (4bits), 3 downto 0 => px_x (4bits)
-signal s_tiles_writting_index : std_logic_vector(10 downto 0);  -- 10 downto 8 => id (3bits), 7 downto 4 => px_y (4bits), 3 downto 0 => px_x (4bits)
+    component tile
+        Port (
+            i_clk         : in  std_logic;
+            i_reset       : in  std_logic;
+            i_x           : in  std_logic_vector(2 downto 0);
+            i_y           : in  std_logic_vector(2 downto 0);
+            o_color_index : out std_logic_vector(3 downto 0);
+            i_we          : in  std_logic;
+            i_wr_x        : in  std_logic_vector(2 downto 0);
+            i_wr_y        : in  std_logic_vector(2 downto 0);
+            i_pixel_data  : in  std_logic_vector(3 downto 0)
+        );
+    end component;
 
--- au final, c'est comme si on faisait cette op√©ration pour get l'index du bits de couleur => index_color_code = (tile_id * 256) + (tile_y * 16) + tile_x, genre comme on le ferait dans une vrai matrice
--- A1   A2   A3   A4
--- A5   A6   A7   A8
--- A9   A10  A11  A12
--- A13  A14  A15  A16
+    type tile_output_array_t is array (0 to 63) of std_logic_vector(3 downto 0);
+    signal tile_outputs : tile_output_array_t;
+    signal selected_color : std_logic_vector(3 downto 0);
 
 begin
 
-    s_tiles_reading_index(10 downto 8) <= i_tile_id(2 downto 0);
-    s_tiles_reading_index(7 downto 4)  <= i_tile_read_px_y;
-    s_tiles_reading_index(3 downto 0)  <= i_tile_read_px_x;
-            
-    s_tiles_writting_index(10 downto 8) <= i_write_tile_id(2 downto 0);
-    s_tiles_writting_index(7 downto 4)  <= i_write_tile_px_y;
-    s_tiles_writting_index(3 downto 0)  <= i_write_tile_px_x;
+    -- Instanciation sÈcurisÈe des tuiles
+    tiles_generator : for i in 0 to 63 generate
 
-    process(i_clk)
-    begin  
---        if i_reset = '1' then
---            s_tile_arr_actor_px <= ( others => (others => '0'));
-        
-        if rising_edge(i_clk) then -- magouille de on feed la couleur, mais on peux edit une couleur en parrall√®le aussi (truss)
-            o_color_code <= s_tile_arr_actor_px(to_integer(unsigned(s_tiles_reading_index))); 
-            
-        
-            if i_write_buffer_px = '1' then
-                s_tile_arr_actor_px(to_integer(unsigned(s_tiles_writting_index))) <= i_write_tile_pixel_color;
+        signal we_active : std_logic;
+
+    begin
+        -- Activation seulement si l'index correspond
+        we_process : process(i_we, i_tile_id)
+        begin
+            if i_we = '1' and i_tile_id = std_logic_vector(to_unsigned(i, 6)) then
+                we_active <= '1';
+            else
+                we_active <= '0';
             end if;
-                     
+        end process;
+
+        tile_inst : tile
+            port map (
+                i_clk         => i_clk,
+                i_reset       => i_reset,
+                i_x           => i_x,
+                i_y           => i_y,
+                o_color_index => tile_outputs(i),
+                i_we          => we_active,
+                i_wr_x        => i_wr_x,
+                i_wr_y        => i_wr_y,
+                i_pixel_data  => i_pixel_data
+            );
+    end generate;
+
+    -- Multiplexage de lecture
+    process(i_clk)
+    begin
+        if rising_edge(i_clk) then
+            selected_color <= tile_outputs(to_integer(unsigned(i_tile_id)));
         end if;
-        
-         
     end process;
+
+    o_color_code <= selected_color;
 
 end Behavioral;
